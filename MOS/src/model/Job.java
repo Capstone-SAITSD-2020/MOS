494ade390a7d3338d163e622c7cbfb32b7ad1690
iyamus <iyamus@gmail.com>
2020-01-22 08:51:46 -0700

    200122_Model_created Staff, Job model

---
+++ MOS/src/model/Job.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Job {
+	String jobId;
+	String jobName;
+
+	public Job(String jobId, String jobName) {
+		super();
+		this.jobId = jobId;
+		this.jobName = jobName;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getJobName() {
+		return jobName;
+	}
+
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+
+}
--- MOS/src/model/Staff.java
+++ MOS/src/model/Staff.java
@@ -2,4 +2,39 @@ package model;
 
 public class Staff {
 
+	String staffId;
+	String staffName;
+	String staffPin;
+
+	public Staff(String staffId, String staffName, String staffPin) {
+		super();
+		this.staffId = staffId;
+		this.staffName = staffName;
+		this.staffPin = staffPin;
+	}
+
+	public String getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(String staffId) {
+		this.staffId = staffId;
+	}
+
+	public String getStaffName() {
+		return staffName;
+	}
+
+	public void setStaffName(String staffName) {
+		this.staffName = staffName;
+	}
+
+	public String getStaffPin() {
+		return staffPin;
+	}
+
+	public void setStaffPin(String staffPin) {
+		this.staffPin = staffPin;
+	}
+
 }
